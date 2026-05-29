74afbb2cec9eb2cd1cd7f29d195e3acf9c8aa61e
Amber Adamson <amber.renee.adamson@gmail.com>
2026-05-29 17:03:22 -0400

    Set up db client module for Prisma.

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,12 +1,5 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../prisma/generated/client";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-const prisma = new PrismaClient({ adapter });
+import { prisma } from "../src/lib/prisma";
 
 async function main() {
   const mode = await prisma.mode.create({
